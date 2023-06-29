82690b3c4d281c92804165f0195839b720327fac
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2023-06-29 02:26:22 -0500

    Working darkmode

--- mui-tutorial/src/App.js
+++ mui-tutorial/src/App.js
@@ -2,20 +2,31 @@ import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
 import Rightbar from "./components/Rightbar";
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, ThemeProvider, createTheme } from "@mui/material";
 import Add from "./components/Add";
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+
   return (
-    <Box>
-      <Navbar></Navbar>
-      <Stack direction="row" spacing={2} justifyContent="space-between">
-        <Sidebar></Sidebar>
-        <Feed></Feed>
-        <Rightbar></Rightbar>
-      </Stack>
-      <Add />
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar></Navbar>
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar mode={mode} setMode={setMode}></Sidebar>
+          <Feed></Feed>
+          <Rightbar></Rightbar>
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
   );
 }
 
--- mui-tutorial/src/components/Sidebar.js
+++ mui-tutorial/src/components/Sidebar.js
@@ -18,7 +18,7 @@ import {
   ModeNight,
 } from "@mui/icons-material";
 
-export default function Sidebar() {
+export default function Sidebar({ mode, setMode }) {
   return (
     <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
       <Box position="fixed">
@@ -84,7 +84,9 @@ export default function Sidebar() {
               <ListItemIcon>
                 <ModeNight />
               </ListItemIcon>
-              <Switch />
+              <Switch
+                onChange={(e) => setMode(mode === "light" ? "dark" : "light")}
+              />
             </ListItemButton>
           </ListItem>
         </List>
